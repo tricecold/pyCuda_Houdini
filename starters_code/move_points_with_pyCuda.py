bb156d029333f66881ec2a2c542338f12a77cf6a
tricecold <tricecold@gmail.com>
2016-02-14 20:15:59 -0500

    Update move_points_with_pyCuda.py

--- starters_code/move_points_with_pyCuda.py
+++ starters_code/move_points_with_pyCuda.py
@@ -8,8 +8,7 @@ import numpy
 
 string_buffer = geo.pointFloatAttribValuesAsString("P", float_type=hou.numericData.Float32)
 
-a_gpu = gpuarray.to_gpu(numpy.fromstring(string_buffer, dtype='float32' ).reshape(-1, 3).astype(numpy.float32))
-a_doubled = (1.2*a_gpu).get()
-##print a_doubled
-geo.setPointFloatAttribValuesFromString("P", numpy.getbuffer(a_doubled), float_type=hou.numericData.Float32)
+a_gpu = gpuarray.to_gpu(numpy.fromstring(string_buffer, dtype='float32' ).reshape(-1, 3).astype(numpy.float32)) ###creates the array
+a_doubled = (1.2*a_gpu).get() ##multiplies the positions by 1.2
+geo.setPointFloatAttribValuesFromString("P", numpy.getbuffer(a_doubled), float_type=hou.numericData.Float32) ## writes back the new positions to points
 
